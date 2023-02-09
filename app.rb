2a399cb174b7cdf41e6ea42532c5c4ec53f9c759
AndRBuZ <andrrrw56@gmail.com>
2023-02-09 18:51:37 +0500

    Lesson 19:time_format

--- app.rb
+++ app.rb
@@ -1,24 +1,37 @@
+require_relative 'time_format'
+
 class App
   def call(env)
-    perform_request
-    [status, headers, body]
+    @env = env
+    if @env['REQUEST_PATH'] == '/time'
+      handle_time_request
+    else
+      send_response("Page not found\n", 404)
+    end
   end
 
   private
-
-  def perform_request
-    sleep rand(2..3)
-  end
-
-  def status
-    200
-  end
 
   def headers
     { 'Content-Type' => 'text/plain' }
   end
 
-  def body
-    ["Welcome aboard!\n"]
+  def handle_time_request
+    formats = take_format
+    time_format = TimeFormat.new(formats)
+    time_format.call
+    if time_format.success?
+      send_response(time_format.time_to_string, 200)
+    else
+      send_response(time_format.unknown_format_answer, 400)
+    end
+  end
+
+  def take_format
+    Rack::Utils.parse_query(@env['QUERY_STRING'])['format']
+  end
+
+  def send_response(body, status)
+    Rack::Response.new(body, status, headers).finish
   end
 end
